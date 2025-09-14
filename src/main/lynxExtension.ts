0ddb5e454a882d58dc57155cfd7976b250d9ffa4
kindabrazy <kindofbrazy@gmail.com>
2025-09-14 14:03:59 +0330

    initial scripts

---
+++ src/main/lynxExtension.ts
@@ -0,0 +1,8 @@
+import {
+  ExtensionMainApi,
+  MainExtensionUtils,
+} from '../../../src/main/Managements/Plugin/Extensions/ExtensionTypes_Main';
+
+export async function initialExtension(_lynxApi: ExtensionMainApi, _utils: MainExtensionUtils) {
+  return;
+}
---
+++ src/renderer/Extension.tsx
@@ -0,0 +1,7 @@
+import './index.css';
+
+import {ExtensionRendererApi} from '../../../src/cross/plugin/ExtensionTypes_Renderer_Api';
+
+export function InitialExtensions(_lynxAPI: ExtensionRendererApi, _extensionId: string) {
+  return;
+}
