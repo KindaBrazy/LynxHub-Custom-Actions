5b064f1b9457f0abbf200aba7b5d3cbbc133d45f
kindabrazy <kindofbrazy@gmail.com>
2025-09-18 09:28:15 +0330

    feat(Modal): add icons to CardType selection and enhance UI rendering

--- src/renderer/Components/Modal/Elements/CardType.tsx
+++ src/renderer/Components/Modal/Elements/CardType.tsx
@@ -2,6 +2,7 @@ import {Select, SelectItem, SharedSelection} from '@heroui/react';
 import {useMemo} from 'react';
 import {useDispatch} from 'react-redux';
 
+import {Play_Icon, Terminal_Icon, Web_Icon} from '../../../../../../src/renderer/src/assets/icons/SvgIcons/SvgIcons';
 import {CustomCardType} from '../../../../cross/CrossTypes';
 import {reducerActions, useCustomActionsState} from '../../../reducer';
 
@@ -19,6 +20,24 @@ export function CardType() {
     }
   };
 
+  const startContent = useMemo(() => {
+    switch (cardType) {
+      case 'executable':
+        return <Play_Icon />;
+      case 'browser':
+        return <Web_Icon />;
+      case 'terminal':
+        return <Terminal_Icon />;
+      case 'terminal_browser':
+        return (
+          <>
+            <Terminal_Icon />
+            <Web_Icon />
+          </>
+        );
+    }
+  }, [cardType]);
+
   return (
     <div className="flex flex-col gap-y-2">
       <span className="text-foreground-600">Select the card type that fits your needs:</span>
@@ -26,17 +45,29 @@ export function CardType() {
         selectionMode="single"
         label="Select Card Type"
         selectedKeys={[cardType]}
+        startContent={startContent}
         onSelectionChange={onSelectionChange}>
-        <SelectItem key="executable" description="Run and manage a program">
+        <SelectItem key="executable" startContent={<Play_Icon />} description="Run and manage a program">
           Executable
         </SelectItem>
-        <SelectItem key="terminal_browser" description="Open a terminal and a browser simultaneously.">
+        <SelectItem
+          startContent={
+            <div>
+              <Terminal_Icon />
+              <Web_Icon />
+            </div>
+          }
+          key="terminal_browser"
+          description="Open a terminal and a browser simultaneously.">
           Terminal & Browser
         </SelectItem>
-        <SelectItem key="browser" description="Open a browser with a custom URL.">
+        <SelectItem key="browser" startContent={<Web_Icon />} description="Open a browser with a custom URL.">
           Browser
         </SelectItem>
-        <SelectItem key="terminal" description="Open a terminal to run custom commands or scripts.">
+        <SelectItem
+          key="terminal"
+          startContent={<Terminal_Icon />}
+          description="Open a terminal to run custom commands or scripts.">
           Terminal
         </SelectItem>
       </Select>
