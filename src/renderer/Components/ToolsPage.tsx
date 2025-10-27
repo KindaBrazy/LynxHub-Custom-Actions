f18ac063ffaa7d0a04dcaf72a2ffd9d620426075
kindabrazy <kindofbrazy@gmail.com>
2025-10-27 17:18:59 +0330

    refactor(renderer): update tools card icon source and remove local file

--- src/renderer/Components/ToolsPage.tsx
+++ src/renderer/Components/ToolsPage.tsx
@@ -3,11 +3,11 @@ import {useDispatch} from 'react-redux';
 
 import {ToolsCard} from '../../../../src/renderer/src/App/Components/Reusable/ToolsCard';
 import {useTabsState} from '../../../../src/renderer/src/App/Redux/Reducer/TabsReducer';
-import icon from '../icon.png';
 import {reducerActions} from '../reducer';
 
 const title: string = 'Custom Actions';
 const description: string = 'Create, customize and manage custom cards with custom scripts, actions.';
+const icon: string = 'https://raw.githubusercontent.com/KindaBrazy/LynxHub-Custom-Actions/refs/heads/metadata/icon.png';
 
 export default function ToolsPage() {
   const activeTab = useTabsState('activeTab');
