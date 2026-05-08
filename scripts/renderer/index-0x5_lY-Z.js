ad45f77b934be089047bc6a2d0045c402fcf25e7
KindaBrazy <kindofbrazy@gmail.com>
2026-05-08 09:20:53 +0330

    0.3.1

--- scripts/renderer/index-0x5_lY-Z.js
+++
@@ -1,5 +0,0 @@
-import { d as domAnimation } from './features-animation-Dun0TQy2.js';
-
-var index_default = domAnimation;
-
-export { index_default as default };
--- scripts/renderer/src-UW24ZMRV-DaXFEqvh.js
+++
@@ -1,5 +0,0 @@
-import { d as domAnimation } from './features-animation-Dun0TQy2.js';
-
-var src_default = domAnimation;
-
-export { src_default as default };
