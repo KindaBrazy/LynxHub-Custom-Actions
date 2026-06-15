981d3877c56eeae66d583931d87c6e3ad44299d2
KindaBrazy <kindofbrazy@gmail.com>
2026-06-15 18:52:15 +0330

    refactor(tools): inline constants for title and description in `ToolsPage`

--- src/renderer/Components/ToolsPage.tsx
+++ src/renderer/Components/ToolsPage.tsx
@@ -4,9 +4,6 @@ import {Widget6} from '@solar-icons/react-perf/BoldDuotone';
 
 import CustomActionsModal from './Modal/CustomActionsModal';
 
-const title: string = 'Custom Actions';
-const description: string = 'Create, customize and manage custom cards with custom scripts, actions.';
-
 export default function ToolsPage() {
   const modalState = useOverlayState();
 
@@ -14,10 +11,10 @@ export default function ToolsPage() {
     <>
       <CustomActionsModal state={modalState} />
       <ToolsCard
-        title={title}
+        title="Custom Actions"
         onPress={modalState.open}
-        description={description}
         icon={<Widget6 className="size-8 text-cyan-500" />}
+        description="Create, customize and manage custom cards with custom scripts, actions."
       />
     </>
   );
