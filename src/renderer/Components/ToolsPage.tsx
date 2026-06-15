e195d48e0a1f8767b76e259e6c73803aaddc67fc
KindaBrazy <kindofbrazy@gmail.com>
2026-06-15 18:51:53 +0330

    refactor(tools): replace static icon URL with `Widget6` component for improved styling and consistency

--- src/renderer/Components/ToolsPage.tsx
+++ src/renderer/Components/ToolsPage.tsx
@@ -1,11 +1,11 @@
 import {useOverlayState} from '@heroui/react';
 import {ToolsCard} from '@lynx/components/ToolsCard';
+import {Widget6} from '@solar-icons/react-perf/BoldDuotone';
 
 import CustomActionsModal from './Modal/CustomActionsModal';
 
 const title: string = 'Custom Actions';
 const description: string = 'Create, customize and manage custom cards with custom scripts, actions.';
-const icon: string = 'https://raw.githubusercontent.com/KindaBrazy/LynxHub-Custom-Actions/refs/heads/metadata/icon.png';
 
 export default function ToolsPage() {
   const modalState = useOverlayState();
@@ -13,7 +13,12 @@ export default function ToolsPage() {
   return (
     <>
       <CustomActionsModal state={modalState} />
-      <ToolsCard icon={icon} title={title} onPress={modalState.open} description={description} />
+      <ToolsCard
+        title={title}
+        onPress={modalState.open}
+        description={description}
+        icon={<Widget6 className="size-8 text-cyan-500" />}
+      />
     </>
   );
 }
